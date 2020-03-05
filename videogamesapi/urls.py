c83751981537fe7fe7904d9b03ebdbb08234c5e3
Gabriel Felippe <gabrielfelippe90@gmail.com>
2020-03-04 22:02:53 -0300

    API schema added

--- videogamesapi/urls.py
+++ videogamesapi/urls.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from django.urls import include, path
 from videogamesapi import views
 from resources import views as resources_views
+from rest_framework.schemas import get_schema_view
+from rest_framework.renderers import JSONOpenAPIRenderer
 from rest_framework import routers
+
+schema_view = get_schema_view(
+	title='Video Games API',
+	version='0.1', 
+	renderer_classes=[JSONOpenAPIRenderer]
+)
 
 router = routers.DefaultRouter()
 
@@ -18,4 +26,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('documentation/', views.documentation, name='documentation'),
+    path('schema', schema_view),
 ]
